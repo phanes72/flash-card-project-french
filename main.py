680f7b094600c667f527b70a9ec20f9d1575b04d
Patrick <phanes72@gmail.com>
2024-05-23 06:44:05 -0600

    Adjusted project layout

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-import tkinter
 from tkinter import *
 
 BACKGROUND_COLOR = "#B1DDC6"
 
 window = Tk()
 window.title("Flashy")
-# window.config(padx=20)
-window.geometry("1000x800")
+window.config(padx=20, pady=50, bg=BACKGROUND_COLOR)
 
-
-canvas = Canvas(window, width=800, height=800)
+canvas = Canvas(window, width=800, height=526)
 card_front_image = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 300, image=card_front_image)
-canvas.grid(row=0, column=1)
+canvas.create_image(400, 263, image=card_front_image)
+canvas.grid(row=0, column=0)
 # canvas.pack()
 
 right_canvas = Canvas(window, width=100, height=100)
