0cf5f18c08d64f30a51e2edcfe3c5d4cd92dbd55
Patrick <phanes72@gmail.com>
2024-06-02 16:09:56 -0600

    Auto flip card to from French to English

--- main.py
+++ main.py
@@ -4,13 +4,30 @@ import random
 
 data = pd.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
-current_card = random.choice(to_learn)
-
 BACKGROUND_COLOR = "#B1DDC6"
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+current_card = {}
+current_card = random.choice(to_learn)
+
+
+def next_card():
+    global current_card
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="white")
+
+
+def flip_card():
+    canvas.config(bg="white", highlightthickness=0)
+    canvas.itemconfig(card_title, text="")
+
+    canvas.itemconfig(card_title, text="English", fill="black")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="black")
+
 
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
@@ -27,16 +44,7 @@ check_image = PhotoImage(file="images/right.png")
 known_button = Button(image=check_image, highlightthickness=0)
 known_button.grid(row=1, column=1)
 
-
-def flip_card():
-    canvas.config(bg="white", highlightthickness=0)
-    canvas.itemconfig(card_title, text="")
-    canvas.itemconfig(card_word, text="")
-
-    canvas.itemconfig(card_title, text="French", fill="black")
-    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
-
-
+next_card()
 window.after(3000, flip_card)
 
 window.mainloop()
