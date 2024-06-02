6eaf53d6761060e4c1a38d9af2328dc6d605419e
Patrick <phanes72@gmail.com>
2024-06-02 15:55:10 -0600

    Auto flip card to from Eng to Fr

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 
 data = pd.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
-print(to_learn)
+current_card = random.choice(to_learn)
 
 BACKGROUND_COLOR = "#B1DDC6"
 
@@ -12,32 +12,31 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-
-def next_card():
-    canvas.config(bg="white", highlightthickness=0)
-
-    canvas.itemconfig(card_word, text="")
-
-    current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French", fill="black")
-    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
-
-
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
-card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
-card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(400, 150, text="English", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text=current_card["English"], font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
 cross_image = PhotoImage(file="images/wrong.png")
-unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
+unknown_button = Button(image=cross_image, highlightthickness=0)
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
-known_button = Button(image=check_image, highlightthickness=0, command=next_card)
+known_button = Button(image=check_image, highlightthickness=0)
 known_button.grid(row=1, column=1)
 
-#next_card()
+
+def flip_card():
+    canvas.config(bg="white", highlightthickness=0)
+    canvas.itemconfig(card_title, text="")
+    canvas.itemconfig(card_word, text="")
+
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+
+
+window.after(3000, flip_card)
 
 window.mainloop()
